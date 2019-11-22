ad94bf2c19c7db81f0c9e238868c0251808086f9
ZZaier <ZZaier@LI-H022-0T01>
2019-11-22 10:16:22 -0500

    third test

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -5,6 +5,7 @@ public class JavaApplication4 {
     public static void main(String[] args) {
         System.out.println("Bonjour "+args[1]+", " +args[0]);    
         System.out.println("Au revoir!"); 
+        System.out.println("Merci!"); 
     }
     
 }
