b31acc56d06bfb84867573a376ce9e2554f89deb
ZZaier <ZZaier@LI-H022-0T01>
2019-11-22 10:04:51 -0500

    secont test

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -4,6 +4,7 @@ public class JavaApplication4 {
 
     public static void main(String[] args) {
         System.out.println("Bonjour "+args[1]+", " +args[0]);    
+        System.out.println("Au revoir!"); 
     }
     
 }
