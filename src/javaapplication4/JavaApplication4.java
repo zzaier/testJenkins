3f641b348a6787c382abc9220c6cf00501e4d392
ZZaier <ZZaier@LI-H022-0T01>
2019-11-22 09:20:26 -0500

    first test

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,9 @@
+package javaapplication4;
+
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour "+args[1]+", " +args[0]);    
+    }
+    
+}
